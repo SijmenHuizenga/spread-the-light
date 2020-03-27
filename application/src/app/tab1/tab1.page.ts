dd84c601c4bd645cdfdf29dd9b91b1a3296cf877
sijmenhuizenga <sijmenhuizenga@gmail.com>
2020-03-27 20:18:46 +0100

    Flashlight button

--- application/src/app/tab1/tab1.page.ts
+++ application/src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Flashlight } from '@ionic-native/flashlight';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,26 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private platform: Platform) {
+
+  }
+
+  toggleFlashlight() {
+    this.platform.ready().then(() => {
+      Flashlight.available().then((isAvailable :boolean) => {
+        if (!isAvailable) {
+          alert("Flashlight not available on this device");
+          return;
+        }
+        Flashlight.switchOn()
+    
+        // switch off after 3 seconds
+        setTimeout(function() {
+          Flashlight.switchOff(); // success/error callbacks may be passed
+        }, 3000);
+      
+      })
+     });
+  }
 
 }
