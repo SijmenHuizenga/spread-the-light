ac2fe0e6e79b5c24515f6916fa3543f7d4e42581
sijmenhuizenga <sijmenhuizenga@gmail.com>
2020-03-29 20:07:06 +0200

    Big brain settings

--- androidapp/app/src/main/java/nl/fsteamdelft/spreadthelight/ui/ReceivingFragment.java
+++ androidapp/app/src/main/java/nl/fsteamdelft/spreadthelight/ui/ReceivingFragment.java
@@ -1,20 +1,22 @@
 package nl.fsteamdelft.spreadthelight.ui;
 
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.MeteringRectangle;
 import android.os.Bundle;
+import android.util.Range;
 import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.camera.camera2.interop.Camera2Interop;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.databinding.ObservableField;
-import androidx.databinding.ObservableInt;
 import androidx.fragment.app.Fragment;
 import com.google.common.util.concurrent.ListenableFuture;
 import nl.fsteamdelft.spreadthelight.R;
@@ -45,14 +47,22 @@ public class ReceivingFragment extends Fragment {
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build();
 
+
         Preview preview = (new Preview.Builder())
                 .setTargetResolution(new Size(640, 480)).build();
         preview.setSurfaceProvider(binding.previewView.getPreviewSurfaceProvider());
 
-        ImageAnalysis imageAnalysis = (new ImageAnalysis.Builder())
+        ImageAnalysis.Builder builder = (new ImageAnalysis.Builder())
                 .setImageQueueDepth(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                .setTargetResolution(new Size(640, 480))
-                .build();
+                .setTargetResolution(new Size(640, 480));
+
+        (new Camera2Interop.Extender<>(builder))
+                .setCaptureRequestOption(CaptureRequest.CONTROL_AWB_REGIONS, new MeteringRectangle[]{new MeteringRectangle(0, 0, 10, 10, 1000)})
+                .setCaptureRequestOption(CaptureRequest.CONTROL_AF_REGIONS, new MeteringRectangle[]{new MeteringRectangle(0, 0, 10, 10, 1000)})
+                .setCaptureRequestOption(CaptureRequest.CONTROL_AE_REGIONS, new MeteringRectangle[]{new MeteringRectangle(0, 0, 10, 10, 1000)})
+                .setCaptureRequestOption(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, new Range(60, 60));
+
+        ImageAnalysis imageAnalysis = builder.build();
 
         imageAnalysis.setAnalyzer(Executors.newSingleThreadExecutor(), new MorseImageAnalyzer(brightness));
 
